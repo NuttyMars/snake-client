ab338cfbdbc9e2748555aa52129b0c2212095afd
Natalia Martian <natalia.martian@gmail.com>
2020-11-04 21:00:30 +0000

    Reafctored handleUserInput function

--- play.js
+++ play.js
@@ -6,19 +6,22 @@ connect();
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function() {
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+ const setupInput = function() {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
 
-  const handleUserInput = process.stdin.on('data', (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  })
+  stdin.on('data', handleUserInput);
 
   return stdin;
-}
+};
 
 setupInput();
