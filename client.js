eb4692110e53a535f99d8d075f5d057cb4052bb4
Natalia Martian <natalia.martian@gmail.com>
2020-11-05 00:52:20 +0000

    Separated input control in its own file, refactored play.js file accordingly. Set up keys to move snake in all four directions

--- client.js
+++ client.js
@@ -14,19 +14,6 @@ const connect = function() {
   conn.on('connect', () => console.log('Successfully connected to game server'));
   conn.on('connect', () => {
     conn.write('Name: NOM');
-    
-    // conn.write('Move: up');
-    // setTimeout(() => conn.write('Move: up'), 50);
-    // setTimeout(() => conn.write('Move: up'), 100);
-    // setTimeout(() => conn.write('Move: up'), 150);
-    // setTimeout(() => conn.write('Move: up'), 200);
-    // setTimeout(() => conn.write('Move: up'), 250);
-    // setTimeout(() => conn.write('Move: up'), 300);
-    // setTimeout(() => conn.write('Move: up'), 350);
-    // setTimeout(() => conn.write('Move: up'), 400);
-    // setTimeout(() => conn.write('Move: up'), 450);
-    // setTimeout(() => conn.write('Move: up'), 500);
-    // setTimeout(() => conn.write('Move: up'), 550);
 
   });
 
